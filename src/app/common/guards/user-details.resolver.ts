babebe36b724f01ddd8e73315d08a2673185aa51
Alina <alina.mihai90@gmail.com>
2017-06-05 10:49:11 +0300

    adjust top bar items

--- src/app/common/guards/user-details.resolver.ts
+++ src/app/common/guards/user-details.resolver.ts
@@ -1,6 +1,8 @@
-import { Injectable }             from '@angular/core';
-import { Router, Resolve, RouterStateSnapshot,
-         ActivatedRouteSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import {
+  Router, Resolve, RouterStateSnapshot,
+  ActivatedRouteSnapshot
+} from '@angular/router';
 import { AuthTokenService, LoginService } from '../services';
 import { User } from '../models/user.model';
 import { Observable } from 'rxjs/Observable';
@@ -9,15 +11,16 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class UserDetailResolver implements Resolve<User> {
   constructor(private authTokenService: AuthTokenService,
-   private loginService: LoginService,
-    private router: Router) {}
+    private loginService: LoginService,
+    private router: Router) { }
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any {
     if (!this.loginService.getCurrentUser() && this.authTokenService.isLoggedIn()) {
-      return  this.loginService.getUserDetails().subscribe((user) => {
-          console.log(user);
+      return this.loginService.getUserDetails().subscribe((user) => {
       });
     }
-    this.router.navigate(['/landing']);
-    return null;
+    else if (!this.loginService.getCurrentUser()) {
+      this.router.navigate(['/landing']);
+      return null;
+    }
   }
 }
